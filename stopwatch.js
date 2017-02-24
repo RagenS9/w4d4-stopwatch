7e3b7a7f7579e5972933e03a60e9cd08f8702dfc
Ragen Sanner <ragens9@att.net>
2017-02-24 16:26:36 -0500

    removed some excess spaces I had in the JS

--- stopwatch.js
+++ stopwatch.js
@@ -9,13 +9,12 @@ var s = 0;
 var m = 0;
 
 var running; // made this in case the trick to the pausing thing was turning off the running function for a while.
-var reset; //made this to deal with extra pause functions, but might not need it?
 
  button.addEventListener('click', function(e){ //putting the function here ties the timer to the event of click, instead of it just starting to count on its own.
      //not sure why we need the e here; I had it here, but Jeff told me that was wrong and that I should delete it. 
     if (e.target.innerHTML === 'Start') {
         e.target.innerHTML = 'Pause';
-        running = setInterval (handlesTimer, 10);
+        running = setInterval(handlesTimer, 100);
     }
 
     else if (e.target.innerHTML === 'Pause') {
@@ -26,31 +25,22 @@ var reset; //made this to deal with extra pause functions, but might not need it
 
     else if (e.target.innerHTML === 'Resume') {
         e.target.innerHTML = 'Pause';
-        running = setInterval (handlesTimer, 10);
+        running = setInterval(handlesTimer, 100);
     }
 }); 
-
-//resetting after 15 seconds to 0, called the function in first else if for single click.
-function reset () {
-    ms = 0;
-    s = 0;
-    m = 0;
-    button.innerText='Start';
-    timer.innerText='00:00:00';
-};
 
 //timer setup
 function handlesTimer() {
     ms++;
     timer.innerHTML = `<strong>${m}:${s}:${ms}</strong>`;
 
-    if (ms >= 99) {
+    if (ms >= 10) {
         s++;
         ms=0;
-        setColor(); // call the color change function that I wrote beneath this funtion.
+        setColor(); // call the color change function that I wrote at the end of the file.
     }
 
-    else if (s >= 59) {
+    if (s >= 60) {
         m++;
         s=0;
     }
@@ -67,6 +57,15 @@ button.addEventListener('dblclick', function(e){
         timer.innerHTML = '00:00:00';
     }
 });
+
+//resetting after 15 seconds in Pause mode to 0, called the function in first else if for single click.
+function reset() {
+    ms = 0;
+    s = 0;
+    m = 0;
+    button.innerText='Start';
+    timer.innerText='00:00:00';
+};
 
 // define the random color changes
 function setColor() { 
